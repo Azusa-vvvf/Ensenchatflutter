0daef5c532dc255f6a4c3ca3f0632c83e85e3f45
Mamougata_Furina <ogateru0107@outlook.jp>
2024-03-11 19:54:41 +0900

    通知実装

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,14 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'dart:async';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:io';
+
+
+
 
 const locale = Locale("ja", "JP");
 
@@ -27,8 +35,108 @@ const MaterialColor EnsenColor = const MaterialColor(
   },
 );
 
-void main() {
+/*void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final messagingInstance = FirebaseMessaging.instance;
+  messagingInstance.requestPermission();
+
+  final fcmToken = await messagingInstance.getToken();
+  debugPrint('FCM TOKEN: $fcmToken');
+
   runApp(MyApp());
+}*/
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final messagingInstance = FirebaseMessaging.instance;
+
+  final fcmToken = await messagingInstance.getToken();
+  debugPrint('FCM TOKEN: $fcmToken');
+
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  if (Platform.isAndroid) {
+    final androidImplementation =
+    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>();
+    await androidImplementation?.createNotificationChannel(
+      const AndroidNotificationChannel(
+        'default_notification_channel',
+        '更新通知',
+        importance: Importance.max,
+      ),
+    );
+    await androidImplementation?.requestNotificationsPermission();
+  }
+
+  // 通知設定の初期化を行う
+  _initNotification();
+
+  // アプリ停止時に通知をタップした場合はgetInitialMessageでメッセージデータを取得できる
+  final message = await FirebaseMessaging.instance.getInitialMessage();
+  // 取得したmessageを利用した処理などを記載する
+
+  runApp(MyApp());
+}
+
+Future<void> _initNotification() async {
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    // バックグラウンド起動中に通知をタップした場合の処理
+  });
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+    final notification = message.notification;
+    final android = message.notification?.android;
+
+    // フォアグラウンド起動中に通知が来た場合の処理
+
+    // フォアグラウンド起動中に通知が来た場合、
+    // Androidは通知が表示されないため、ローカル通知として表示する
+    // https://firebase.flutter.dev/docs/messaging/notifications#application-in-foreground
+    if (Platform.isAndroid) {
+      // プッシュ通知をローカルから表示する
+      await FlutterLocalNotificationsPlugin().show(
+        0,
+        notification!.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            'default_notification_channel',
+            '更新通知',
+            importance: Importance.max, // 通知の重要度の設定
+            icon: android?.smallIcon,
+          ),
+        ),
+        payload: json.encode(message.data),
+      );
+    }
+  });
+
+  // ローカルから表示したプッシュ通知をタップした場合の処理を設定
+  flutterLocalNotificationsPlugin.initialize(
+    const InitializationSettings(
+      android: AndroidInitializationSettings(
+          '@mipmap/ic_launcher'), //通知アイコンの設定は適宜行ってください
+      iOS: DarwinInitializationSettings(),
+    ),
+    onDidReceiveNotificationResponse: (details) {
+      if (details.payload != null) {
+        final payloadMap =
+        json.decode(details.payload!) as Map<String, dynamic>;
+        debugPrint(payloadMap.toString());
+      }
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
